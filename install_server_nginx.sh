498458b9fb87885280dd3443757333a495117195
widaryanto <wiidaryanto@gmail.com>
2019-10-31 22:55:54 +0700

    Update install_server_nginx.sh

--- install_server_nginx.sh
+++ install_server_nginx.sh
@@ -1,10 +1,36 @@
 #!/bin/bash
-echo "Installing php 7.4, please wait..."
+# ======================= install php 7.4 =======================
 dnf -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
 dnf -y install https://rpms.remirepo.net/enterprise/remi-release-8.rpm
 dnf update
 dnf -y module install php:remi-7.4
-dnf -y install php-fpm php-zip php-gd php-mbstring php-curl php-xml php-pear php-bcmath php-json
+dnf -y install php-fpm php-zip php-gd php-mbstring php-curl php-xml php-pear php-bcmath php-json tar
+
 systemctl enable php-fpm
 systemctl start php-fpm
 php -v
+
+# ======================= install nginx =======================
+dnf -y install nginx
+rm -f /etc/nginx/nginx.conf
+curl -o /etc/nginx/nginx.conf https://raw.githubusercontent.com/widaryanto/webserver/master/nginx.conf
+
+systemctl enable nginx
+systemctl start nginx
+nginx -v
+
+# ======================= setting firewall =======================
+dnf -y install firewalld
+systemctl enable firewalld
+systemctl start firewalld
+
+firewall-cmd --permanent --zone=public --add-service=http
+firewall-cmd --permanent --zone=public --add-service=https
+systemctl reload firewalld
+
+# ======================= finishing install =======================
+chown nginx:nginx /var/www/html -R
+dnf clean all
+systemctl restart nginx php-fpm
+php -v
+nginx -v
