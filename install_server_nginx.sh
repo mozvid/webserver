4555188ccc91ce2b92cccd12bdb85f621bc80754
widaryanto <wiidaryanto@gmail.com>
2019-10-31 22:41:29 +0700

    Update install_server_nginx.sh

--- install_server_nginx.sh
+++ install_server_nginx.sh
@@ -36,37 +36,7 @@ function ee_lib_echo_fail()
 ee_lib_echo_text "Updating, please wait..."
 dnf -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
 dnf -y install https://rpms.remirepo.net/enterprise/remi-release-8.rpm
-dnf update
-dnf -y install wget tar
-
-# Execute: installing
-ee_lib_echo_text "Installing Web Server, please wait..."
-dnf -y install nginx
-systemctl enable nginx
-systemctl start nginx
-
-rm -f /etc/nginx/nginx.conf
-curl -o /etc/nginx/nginx.conf https://raw.githubusercontent.com/widaryanto/webserver/master/nginx.conf
-
-ee_lib_echo "Installing Firewall, please wait..."
-dnf -y install firewalld
-systemctl enable firewalld
-systemctl start firewalld
-firewall-cmd --permanent --zone=public --add-service=http
-firewall-cmd --permanent --zone=public --add-service=https
-systemctl reload firewalld
-
-ee_lib_echo_text "Installing php 7.4, please wait..."
 dnf -y module install php:remi-7.4
 dnf -y install php-fpm php-zip php-gd php-mbstring php-curl php-xml php-pear php-bcmath php-json
 systemctl enable php-fpm
 systemctl start php-fpm
-
-dnf clean all
-chown nginx:nginx /var/www/html -R
-systemctl restart nginx php-fpm
-#clear
-
-ee_lib_echo "Installing Finished"
-php -v
-nginx -v
