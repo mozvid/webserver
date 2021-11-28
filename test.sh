098d4a71dd56c4ee68d3abeba539aa1b7166170e
mozvid <wiidaryanto@gmail.com>
2021-11-28 18:09:21 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,21 +1,9 @@
 #!/bin/bash
-echo "gass"
-while [ true ] ;
+echo "kosong"
+
+while [-z "$server"];
 do
 read -p "Enter Your Machine Name : " server
-if [[ ! -z "${server}" ]];
-then
-	printf "$server\n"
-break;
-fi
-done
-
-echo it starts
-
-i=1
-while [ $i -le 5 ]; do
-  echo i is $i
-  i=$(( i+1 ))
 done
 
 echo "kosong"
