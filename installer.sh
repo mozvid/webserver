0ec4e4e51c07db27439029682290a88b0a9280a7
mozvid <wiidaryanto@gmail.com>
2021-11-28 14:39:33 +0700

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -9,7 +9,7 @@ dnf -y install php-fpm php-cli php-mysqlnd php-pecl-memcached php-common php-zip
 systemctl start php-fpm
 systemctl enable php-fpm
 php -v
-sleep 3s
+sleep 5s
 
 printf "$(tput setaf 2)======================= install nginx =======================\n$(tput sgr0)"
 dnf -y install nginx
@@ -17,7 +17,7 @@ dnf -y install nginx
 systemctl start nginx
 systemctl enable nginx
 nginx -v
-sleep 3s
+sleep 5s
 
 printf "$(tput setaf 2)======================= install memcached =======================\n$(tput sgr0)"
 dnf -y install memcached libmemcached
@@ -25,7 +25,7 @@ dnf -y install memcached libmemcached
 systemctl start memcached
 systemctl enable memcached
 #systemctl status memcached
-sleep 3s
+sleep 5s
 
 printf "$(tput setaf 2)======================= install crontabs =======================\n$(tput sgr0)"
 dnf -y install crontabs
@@ -36,6 +36,7 @@ systemctl enable crond.service
 crontab <<EOF
 */2 * * * * /root/autorestart.sh
 EOF
+sleep 5s
 
 printf "$(tput setaf 2)======================= install mysql =======================\n$(tput sgr0)"
 dnf -y install mysql-server
@@ -55,7 +56,7 @@ EOF
 systemctl set-environment MYSQLD_OPTS="--skip-log-bin=1"
 systemctl daemon-reload
 systemctl restart mysqld
-sleep 3s
+sleep 5s
 
 printf "$(tput setaf 2)======================= setting firewall =======================\n$(tput sgr0)"
 dnf -y install firewalld
@@ -67,7 +68,7 @@ firewall-cmd --permanent --zone=public --add-service=https
 firewall-cmd --permanent --zone=public --add-port=3306/tcp
 firewall-cmd --permanent --zone=public --add-port=11211/tcp  
 systemctl reload firewalld
-sleep 3s
+sleep 5s
 
 printf "$(tput setaf 2)======================= finishing install =======================\n$(tput sgr0)"
 chown -R nginx:nginx /var/www/html
@@ -75,32 +76,32 @@ chown -R nginx:nginx /var/lib/php
 chmod -R 777 /var/www/html
 setsebool -P httpd_can_network_connect 1
 setsebool -P httpd_unified 1
-sleep 3s
+sleep 5s
 
 printf "$(tput setaf 2)======================= Update nginx.conf =======================\n$(tput sgr0)"
 rm -f /etc/nginx/nginx.conf
 curl -o /etc/nginx/nginx.conf https://raw.githubusercontent.com/mozvid/webserver/master/nginx.conf
-sleep 3s
+sleep 5s
 
 printf "$(tput setaf 2)======================= Update php.conf =======================\n$(tput sgr0)"
 rm -f /etc/nginx/default.d/php.conf
 curl -o /etc/nginx/default.d/php.conf https://raw.githubusercontent.com/mozvid/webserver/master/php.conf
-sleep 3s
+sleep 5s
 
 printf "$(tput setaf 2)======================= Update mysql-server.cnf =======================\n$(tput sgr0)"
 rm -f /etc/my.cnf.d/mysql-server.cnf
 curl -o /etc/my.cnf.d/mysql-server.cnf https://raw.githubusercontent.com/mozvid/webserver/master/mysql-server.cnf
-sleep 3s
+sleep 5s
 
 printf "$(tput setaf 2)======================= Update crontabs =======================\n$(tput sgr0)"
 curl -o /root/autorestart.sh https://raw.githubusercontent.com/mozvid/webserver/master/autorestart.sh
 chmod u+x /root/autorestart.sh
-sleep 3s
+sleep 5s
 
 printf "$(tput setaf 2)======================= Update fastcgi.conf =======================\n$(tput sgr0)"
 rm -f /etc/nginx/fastcgi_params
 curl -o /etc/nginx/fastcgi_params https://raw.githubusercontent.com/mozvid/webserver/master/fastcgi_params
-sleep 3s
+sleep 5s
 
 # printf "$(tput setaf 2)======================= Update www.conf =======================\n$(tput sgr0)"
 # rm -f /etc/php-fpm.d/www.conf
@@ -108,9 +109,9 @@ sleep 3s
 # sleep 3s
 
 curl -o /var/www/html/info.php https://raw.githubusercontent.com/mozvid/webserver/master/info.php
-sleep 3s
+sleep 5s
 curl -o db-manager.php -OL  https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1-en.php
-sleep 3s
+sleep 5s
 
 printf "$(tput setaf 2)========================= install finished ========================= \n$(tput sgr0)"
 
