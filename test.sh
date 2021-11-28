47b40cb66d6df4cca5db4c08cd66824bc9ce9558
mozvid <wiidaryanto@gmail.com>
2021-11-28 18:03:23 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-#!/bin/csh
+#!/bin/bash
 echo "gass"
 while [ true ] ;
 do
@@ -8,6 +8,14 @@ then
 	printf "$server\n"
 break;
 fi
+done
+
+echo it starts
+
+i=1
+while [ $i -le 5 ]; do
+  echo i is $i
+  i=$(( i+1 ))
 done
 
 echo "kosong"
